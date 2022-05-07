15bee20432cd1fd558a6565dce618cdb86494a67
MCorange99 <79301995+MCorange99@users.noreply.github.com>
2022-05-07 11:10:47 +0300

    Update help.js

--- src/commands/info/help.js
+++ src/commands/info/help.js
@@ -106,7 +106,7 @@ module.exports = class HelpCommand extends Command {
 
       embed.addField(
         '**Links**', 
-        '**[Invite Me](https://discord.com/api/oauth2/authorize?client_id=837371090783174696&permissions=0&redirect_uri=https%3A%2F%2Fdiscord.events.stdlib.com%2Fdiscord%2Fauth%2F&scope=bot) | ' +
+        '**[Invite Me](https://discord.com/oauth2/authorize?client_id=837371090783174696&permissions=261993005047&scope=bot%20applications.commands) | ' +
         '[Support Server](https://discord.gg/M7nDZxKk24) | ' +
         '[Repository](https://github.com/KeithosTeam/Keithos)**'
       );
